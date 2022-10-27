aaef0a828c65a8de6e47620e703440522dec2c2f
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-10-27 11:00:26 +0000

    Fix dndbeyond.com

--- src/data/rules.js
+++ src/data/rules.js
@@ -15313,6 +15313,7 @@ const rules = {
   "coquedetelephone.fr": { j: "3" },
   "de-vogel.nl": { j: "6" },
   "huizenzoeker.nl": { j: "6" },
+  "dndbeyond.com": { j: "6" },
 };
 
 const blockUrls = {
