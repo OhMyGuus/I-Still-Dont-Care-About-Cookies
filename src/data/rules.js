75a06b202d42663895f651ed771b6065279eabaf
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-10-27 12:40:30 +0000

    Fix jw.org
    #229

--- src/data/rules.js
+++ src/data/rules.js
@@ -15313,6 +15313,7 @@ const rules = {
   "coquedetelephone.fr": { j: "3" },
   "de-vogel.nl": { j: "6" },
   "huizenzoeker.nl": { j: "6" },
+  "jw.org": { s: ".lnc-firstRunPopup{display:none !important}" },
 };
 
 const blockUrls = {
