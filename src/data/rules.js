acf38fa9e44a3843ba86b996f770840dfa895b0f
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-10-31 18:44:28 +0000

    Fix fresh-pool.de
    fixes #302

--- src/data/rules.js
+++ src/data/rules.js
@@ -15441,6 +15441,7 @@ const rules = {
   "fakenewsfrance.com": { s: "#js-cc-modal{display:none !important} html{overflow-y: unset !important}" },
   "123inkt.nl": { s: ".simple-popup-container{display:none !important}" },
   "beruby.com": { s: "#box-shadow{display:none !important}" },
+  "fresh-pool.de": { c: "77" },
 };
 
 const blockUrls = {
