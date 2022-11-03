e74e637d548816ae9abb445f2639825d46ed1c03
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-03 07:10:53 +0000

    Fix etiskarna.si
    fixes #324

--- src/data/rules.js
+++ src/data/rules.js
@@ -15459,6 +15459,7 @@ const rules = {
   "elektronik-lavpris.dk": { j: "6" },
   "yes2move.com": { c: "34" },
   "zugportal.de": { j: "3" },
+  "etiskarna.si": { s: ".cookie__content{display:none !important}" },
 };
 
 const blockUrls = {
