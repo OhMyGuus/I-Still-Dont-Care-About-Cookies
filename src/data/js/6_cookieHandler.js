be22647e2cb17b8d37e28fea4b8c9b427a847456
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-03 07:29:05 +0000

    Fix archerresourcing.co.uk

--- src/data/js/6_cookieHandler.js
+++ src/data/js/6_cookieHandler.js
@@ -499,6 +499,10 @@ function getE(hostname) {
       ];
     case "elektronik-lavpris.dk":
       return ["cookieconsent=necessary"];
+    case "archerresourcing.co.uk":
+      return [
+        "fs-cc=%257B%2522consents%2522%253A%257B%2522analytics%2522%253Afalse%252C%2522essential%2522%253Atrue%252C%2522marketing%2522%253Afalse%252C%2522personalization%2522%253Afalse%252C%2522uncategorized%2522%253Afalse%257D%257D",
+      ];  
   }
 
   const parts = hostname.split(".");
