d8aefa9d45ad817ff2376d60bd1eb19ffff5c853
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-06 19:12:32 +0000

    Fix lvvwd.com
    fixes #381 
    fixes #382

--- src/data/rules.js
+++ src/data/rules.js
@@ -15469,6 +15469,7 @@ const rules = {
   "roland-rechtsschutz.de": { j: "5" },
   "honda.co.za": { s: ".cookies{display:none !important}" },
   "dlive.tv": { j: "5" },
+  "lvvwd.com": { s: "#PolicyNotice{display:none !important}" },
 };
 
 const blockUrls = {
