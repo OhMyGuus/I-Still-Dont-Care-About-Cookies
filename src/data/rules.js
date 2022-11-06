eee96f81dd2e8e21d420dc9262fbae2725c9b3a8
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-06 20:52:50 +0000

    Added support for marcos.com (#392)
    fixes #378

--- src/data/rules.js
+++ src/data/rules.js
@@ -15469,6 +15469,7 @@ const rules = {
   "roland-rechtsschutz.de": { j: "5" },
   "honda.co.za": { s: ".cookies{display:none !important}" },
   "dlive.tv": { j: "5" },
+  "marcos.com": { s: ".policy-alert{display:none !important}" },
   "pravniprostor.cz": { s: ".CookiesNoticestyle__Wrapper-sc-1vzpzm5-0{display:none !important}" },
   "bev.gv.at": { s: 'div[class="MuiBox-root css-1z0mgm7"]{display:none !important}' },
   "candis.io": { j: "6" },
