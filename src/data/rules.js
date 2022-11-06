5f4ef1052d6d06c6ed35921350cfb4bca9b42236
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-06 20:52:20 +0000

    Added support for pravniprostor.cz (#375)
    fixes #358

--- src/data/rules.js
+++ src/data/rules.js
@@ -15469,6 +15469,7 @@ const rules = {
   "roland-rechtsschutz.de": { j: "5" },
   "honda.co.za": { s: ".cookies{display:none !important}" },
   "dlive.tv": { j: "5" },
+  "pravniprostor.cz": { s: ".CookiesNoticestyle__Wrapper-sc-1vzpzm5-0{display:none !important}" },
   "bev.gv.at": { s: 'div[class="MuiBox-root css-1z0mgm7"]{display:none !important}' },
   "candis.io": { j: "6" },
   "kauppakeskuswilla.fi": { s: ".cdk-global-overlay-wrapper{display:none !important}" },
