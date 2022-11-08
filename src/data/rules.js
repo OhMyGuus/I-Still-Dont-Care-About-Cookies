0e6abd7a5d1628fd7f8ec16fc48060868eaf7934
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-08 21:08:31 +0000

    Fix kampanje.com
    fixes #434

--- src/data/rules.js
+++ src/data/rules.js
@@ -15523,6 +15523,7 @@ const rules = {
   "eneba.com": {
     s: "#app > footer ~ div, main > div + div + div:last-child{display:none !important}",
   },
+  "kampanje.com": { s: ".adn-window{display:none !important}" },
 };
 
 const blockUrls = {
