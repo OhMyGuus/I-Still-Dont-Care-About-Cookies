12db6b4f91b6d23f6087666126e2147f05415a7f
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-09 18:58:36 +0000

    Fix pzkosz.pl
    fixes #457

--- src/data/rules.js
+++ src/data/rules.js
@@ -15527,6 +15527,7 @@ const rules = {
   "cryptomaan.eu": { s: ".bottom-0{display:none !important}" },
   "dotnetconf.net": { s: "#cookie-consent{display:none !important}" },
   "wenance.com": { s: ".js-consent{display:none !important}" },
+  "pzkosz.pl": { s: ".zgoda_na_przetwarzanie{display:none !important}" },
 };
 
 const blockUrls = {
