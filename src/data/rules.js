f2c3c946f2a5f06ce9a7e4a001d0346632f84c09
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-12 08:04:36 +0000

    Fix eberry.cz
    fixes #508

--- src/data/rules.js
+++ src/data/rules.js
@@ -15533,6 +15533,7 @@ const rules = {
   "pzkosz.pl": { s: ".zgoda_na_przetwarzanie{display:none !important}" },
   "kei.pl": { j: "6" },
   "blim.com": { s: "#blim-header > blim-header-message:first-child{display:none !important}" },
+  "eberry.cz": { s: ".c-cookie-consent-bar{display:none !important}" },
 };
 
 const blockUrls = {
