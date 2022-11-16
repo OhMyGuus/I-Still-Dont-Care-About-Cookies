621b0fd23432cc65599a777bff41397ed51f2513
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-16 11:51:36 +0000

    Fix xxxlesnina.si
    fixes #572

--- src/data/rules.js
+++ src/data/rules.js
@@ -15560,6 +15560,7 @@ const rules = {
   "ludonauta.es": { c: "77" },
   "oponeo.co.uk": { s: "body > form + div{display:none !important} .noScroll{overflow: unset !important}" },
   "herbata.info": { s: "#elementor-popup-modal-3197{display:none !important}" },
+  "xxxlesnina.si": { s: "footer + div{display:none !important}" },
 };
 
 const blockUrls = {
