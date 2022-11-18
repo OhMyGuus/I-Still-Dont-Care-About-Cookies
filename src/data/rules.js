c97d4180e99da2817604a26ca10fe7203d74b439
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-18 15:23:33 +0000

    Fix encazip.com
    fixes #603

--- src/data/rules.js
+++ src/data/rules.js
@@ -15617,6 +15617,7 @@ const rules = {
   "supraphonline.cz": {
     s: 'div[style*="position: fixed; bottom: 0;"]{display:none !important}',
   },
+  "encazip.com": { s: ".eHzdtJ{display:none !important}" },
 };
 
 const blockUrls = {
