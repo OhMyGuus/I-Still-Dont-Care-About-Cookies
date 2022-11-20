353a9a7644cfba7d1ba99ef9942dce75849aa97f
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-20 12:33:13 +0000

    Fix doratoon.com
    fixes #639

--- src/data/rules.js
+++ src/data/rules.js
@@ -15622,6 +15622,7 @@ const rules = {
   "encazip.com": { s: ".eHzdtJ{display:none !important}" },
   "thomas-elliott.co.uk": { c: "34" },
   "landrover.co.jp": { s: ".notificationBarsWrapper{display:none !important}" },
+  "doratoon.com": { s: ".cookie-box{display:none !important}" },
 };
 
 const blockUrls = {
