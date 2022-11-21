1f66ef27215a2eda17072ddd1954a80609ecf9e5
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-21 15:21:21 +0000

    Fix jeancoutu.com
    fixes #674

--- src/data/rules.js
+++ src/data/rules.js
@@ -15660,6 +15660,7 @@ const rules = {
   },
   "imaxchina.com": { s: ".ly_cookie{display:none !important}" },
   "icanw.org": { s: "#disclaimer{display:none !important}" },
+  "jeancoutu.com": { s: "#cookie-consent{display:none !important}" },
 };
 
 const blockUrls = {
