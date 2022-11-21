942269b85807e7070c92766dc81f2e8e58be9f42
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-21 15:17:44 +0000

    Fix airteam.eu
    fixes #673

--- src/data/rules.js
+++ src/data/rules.js
@@ -15660,6 +15660,7 @@ const rules = {
   },
   "imaxchina.com": { s: ".ly_cookie{display:none !important}" },
   "icanw.org": { s: "#disclaimer{display:none !important}" },
+  "airteam.eu": { s: ".modal-backdrop{display:none !important} .modal-open{overflow: unset !important}" },
 };
 
 const blockUrls = {
