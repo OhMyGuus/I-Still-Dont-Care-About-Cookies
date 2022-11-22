48011e955ad6766611a681cdd93b1af47f6d81de
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-22 16:53:13 +0000

    Fix payhawk.com
    fixes #688

--- src/data/rules.js
+++ src/data/rules.js
@@ -15660,6 +15660,7 @@ const rules = {
   },
   "imaxchina.com": { s: ".ly_cookie{display:none !important}" },
   "icanw.org": { s: "#disclaimer{display:none !important}" },
+  "payhawk.com": { s: ".wookiee-wrapper{display:none !important}" },
 };
 
 const blockUrls = {
