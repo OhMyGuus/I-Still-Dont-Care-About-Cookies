6bb8487ef2febb302a75e3463b2aa0d18342a835
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-22 17:07:22 +0000

    Fix comparethemarket.com
    fixes #693

--- src/data/rules.js
+++ src/data/rules.js
@@ -15660,6 +15660,7 @@ const rules = {
   },
   "imaxchina.com": { s: ".ly_cookie{display:none !important}" },
   "icanw.org": { s: "#disclaimer{display:none !important}" },
+  "comparethemarket.com": { s: "#__tealiumGDPRcpPrefs{display:none !important}" },
 };
 
 const blockUrls = {
