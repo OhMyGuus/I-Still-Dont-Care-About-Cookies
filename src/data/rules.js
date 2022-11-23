667e36e70b2d9bd3d04468929289a1ed340b45a6
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-23 19:12:43 +0000

    Fix repubblica.it
    fixes #719

--- src/data/rules.js
+++ src/data/rules.js
@@ -15660,6 +15660,7 @@ const rules = {
   },
   "imaxchina.com": { s: ".ly_cookie{display:none !important}" },
   "icanw.org": { s: "#disclaimer{display:none !important}" },
+  "repubblica.it": { s: "#iubenda-cs-banner{display:none !important}" },
 };
 
 const blockUrls = {
