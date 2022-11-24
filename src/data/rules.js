f152d2f8980d8e521c029d8efad4e236b7f6644f
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-24 00:38:09 +0000

    Added support for payhawk.com (#700)
    fixes #688

--- src/data/rules.js
+++ src/data/rules.js
@@ -15660,6 +15660,7 @@ const rules = {
   },
   "imaxchina.com": { s: ".ly_cookie{display:none !important}" },
   "icanw.org": { s: "#disclaimer{display:none !important}" },
+  "payhawk.com": { s: ".wookiee-wrapper{display:none !important}" },
   "apfelpage.de": { s: "#ccm-widget{display:none !important} .ccm-blocked{overflow: unset !important}" },
   "inlingua-muenchen.de": { s: "#ccm-widget{display:none !important} .ccm-blocked{overflow: unset !important}" },
   "airteam.eu": { s: ".modal-backdrop{display:none !important} .modal-open{overflow: unset !important}" },
