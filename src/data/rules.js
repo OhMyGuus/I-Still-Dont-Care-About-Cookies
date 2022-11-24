5ed1a334780ba3da3e821e6152892c24830e76b0
appeasementPolitik <108810900+appeasementPolitik@users.noreply.github.com>
2022-11-24 00:39:37 +0000

    Added support for comparethemarket.com (#705)
    fixes #693

--- src/data/rules.js
+++ src/data/rules.js
@@ -15660,6 +15660,7 @@ const rules = {
   },
   "imaxchina.com": { s: ".ly_cookie{display:none !important}" },
   "icanw.org": { s: "#disclaimer{display:none !important}" },
+  "comparethemarket.com": { s: "#__tealiumGDPRcpPrefs{display:none !important}" },
   "lamnia.com": { s: ".cookie-background{display:none !important}" },
   "payhawk.com": { s: ".wookiee-wrapper{display:none !important}" },
   "apfelpage.de": { s: "#ccm-widget{display:none !important} .ccm-blocked{overflow: unset !important}" },
