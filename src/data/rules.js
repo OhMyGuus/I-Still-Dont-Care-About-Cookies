810608eee9e0fe9d2fa26c218d5c4ce0f1fe4a8f
OhMyGuus <info@guus.ninja>
2023-01-29 01:45:41 +0100

    Added support for advantest.com fixes #1259

--- src/data/rules.js
+++ src/data/rules.js
@@ -16078,6 +16078,10 @@ const rules = {
   "nappy.co": { s: ".btn-block.text-center.class-montserrat.showBanner.padding-top-10.padding-bottom-10{display:none !important}" },
   "elmachips.com.br": { c: "0" },
   "goflink.com": { c: "6" },
+  "advantest.com": { c: "3" },
+
+
+
 
 };
 
