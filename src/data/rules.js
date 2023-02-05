f55f2e2df0b26d5fd194109e15dad7479ab4dd95
OhMyGuus <info@guus.ninja>
2023-02-05 03:27:09 +0100

    Added support for trampaboards.com fixes #1440

--- src/data/rules.js
+++ src/data/rules.js
@@ -16141,7 +16141,7 @@ const rules = {
   "cosmosmagazine.com": { s: ".adn-window{display:none !important}" },
   "nicepage.com": { s: "#np-cookie-consent{display:none !important}" },
   "actievandedag.nl": { s: ".fixed.z-20.bottom-0.left-0.right-0.py-4.bg-black.bg-opacity-80{display:none !important}" },
-
+  "trampaboards.com": { s: "#eu_cookie_instructions{display:none !important}" },
 
 
 };
