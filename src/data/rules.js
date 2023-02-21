d1d7e4293d7b8c1cf75b99d9ba06f398982ee69a
OhMyGuus <info@guus.ninja>
2023-02-21 13:33:30 +0100

    Added support for cancerfonden.se fixes #1813

--- src/data/rules.js
+++ src/data/rules.js
@@ -16419,7 +16419,7 @@ const rules = {
   "skyfi.com": { s: "section[class=\"fixed bottom-0 px-20 pb-25 left-0 right-0 lg:right-[unset] lg:p-30 z-[5000]\"]{display:none !important}" },
   "lendwise.com": { c: 14 },
   "nbg.gov.ge": { s: "div[class=\"md:w-61-1 right-0 w-full md:block absolute bottom-0 bg-primary-main md:py-2-1 pt-2-1 pb-4-0 px-3-0 no-print\"]{display:none !important}" },
-
+  "cancerfonden.se": { s: "div[class^=\"CookieDisclaimer__Wrapper\"]{display:none !important}" },
 
 
 
