cc72ce47569ce98f78171064316438293246dbf9
Guus van der Meer <info@guus.ninja>
2023-12-30 21:19:43 +0100

    Merged common.css from original

--- src/data/rules.js
+++ src/data/rules.js
@@ -18503,7 +18503,7 @@ const blockUrls = {
     { r: "/Sticky2,ConsentManager" },
     { r: "cnilCookie.js" },
     { r: "basketballbelieve.com" },
-    { r: "app.usercentrics.eu/browser-ui/", e: ["kicker.de", "kicker.ch"]},
+    { r: "app.usercentrics.eu/browser-ui/", e: ["kicker.de", "kicker.ch"] },
     { r: "cc.anytrack.de/app.js" },
     { r: "/utag/ctm/business-insurance/prod/utag.js" },
     { r: "/GDPRPanelComponent" },
@@ -20584,7 +20584,6 @@ const blockUrls = {
     "saeco.de": ["/cookiewall/"],
     "sage.co.uk": ["/Cookies.js"],
     "liebherr.com": ["/cmp/"],
-
   },
 };
 
