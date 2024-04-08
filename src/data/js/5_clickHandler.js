69dcf47c46396301260be966b2f6835760b433d9
Cedric Skwar <cedric@eyevip.com>
2024-04-08 13:57:38 +0200

    Added support for druhotnesuroviny.cz

--- src/data/js/5_clickHandler.js
+++ src/data/js/5_clickHandler.js
@@ -8387,6 +8387,8 @@ function getSelector(host) {
         '.modal[style*="block"] .modal-footer input',
         '.modal[style*="block"] button[id*="AcceptButton"]'
       );
+    case "druhotnesuroviny.cz":
+      return 'a.js-cc-banner-confirmAll-btn:nth-child(2)';
     case "yougov.com":
       return '.cdk-overlay-container yg-tcf-dialog [data-cy="disagree-button"]';
     case "fiveguys.nl":
