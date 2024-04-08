f5234d42e139b1252b6d14d3b7eb3a6c08d11166
Cedric Skwar <cedric@eyevip.com>
2024-04-08 14:40:05 +0200

    Added support for www.rugbycoachweekly.net

--- src/data/js/5_clickHandler.js
+++ src/data/js/5_clickHandler.js
@@ -8387,6 +8387,8 @@ function getSelector(host) {
         '.modal[style*="block"] .modal-footer input',
         '.modal[style*="block"] button[id*="AcceptButton"]'
       );
+    case "rugbycoachweekly.net":
+      return '#aos-Cookie-Modal-Accept';
     case "yougov.com":
       return '.cdk-overlay-container yg-tcf-dialog [data-cy="disagree-button"]';
     case "fiveguys.nl":
