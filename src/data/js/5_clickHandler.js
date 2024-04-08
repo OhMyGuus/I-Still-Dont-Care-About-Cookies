765a0f783c0b7c6d2d9c8420c45fd1bd788203d8
Cedric Skwar <cedric@eyevip.com>
2024-04-08 13:33:19 +0200

    Added support for rockhard.de

--- src/data/js/5_clickHandler.js
+++ src/data/js/5_clickHandler.js
@@ -8387,6 +8387,8 @@ function getSelector(host) {
         '.modal[style*="block"] .modal-footer input',
         '.modal[style*="block"] button[id*="AcceptButton"]'
       );
+    case "rockhard.de":
+      return '.sp_choice_type_11';
     case "yougov.com":
       return '.cdk-overlay-container yg-tcf-dialog [data-cy="disagree-button"]';
     case "fiveguys.nl":
