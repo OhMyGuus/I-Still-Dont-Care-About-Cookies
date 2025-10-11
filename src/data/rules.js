0e151c063dc3da82da39b7686d1750b9111d35ca
Guus van der Meer <info@guus.ninja>
2025-10-11 19:24:29 +0200

    changed to network rule

--- src/data/rules.js
+++ src/data/rules.js
@@ -18513,9 +18513,6 @@ const rules = {
   "careers.teenage.engineering": {
     s: 'dialog[class*="z-cookie-alert"], [class*="z-cookie-alert"], [class*="backdrop:bg-"], [class*="cookie-consent"], [id*="cookie"] { display: none !important; } html, body { overflow: auto !important; }',
   },
-  "nd.nl": {
-    s: "#cookieconsent, #consentcontainer, #cookie_settings, .cookie-banner, #poool-widget-backdrop, #poool-widget-fullpage, #poool-widget-floating { display: none !important; }",
-  },
 
   // end of const rules
 };
@@ -18610,6 +18607,7 @@ const blockUrls = {
   ],
   common_groups: {
     cookie: [
+      { r: "Pubble.CookieManager.js" },
       { r: "/media/plg_system_cookieconfirm" },
       { r: "cookie_meldung.js" },
       { r: "cookiewarning.js" },
