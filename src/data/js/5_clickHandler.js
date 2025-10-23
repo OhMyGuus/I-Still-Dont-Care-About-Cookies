7c6e5bc2ed1584137026443eb32c62f48e3da250
Guus van der Meer <info@guus.ninja>
2025-10-23 15:41:47 +0200

    Changed to click function

--- src/data/js/5_clickHandler.js
+++ src/data/js/5_clickHandler.js
@@ -8369,6 +8369,8 @@ function getSelector(host) {
       return '.modal[style*="block"] #button_accept_cookies';
     case "linasmatkasse.se":
       return "div#cookies button#accept-selected-cookies-btn";
+    case "berliner-sparkasse.de":
+      return "a[data-form='.eprivacy_optin_decline']";
   }
 
   if (host.parts.length > 2) {
