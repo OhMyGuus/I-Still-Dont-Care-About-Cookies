5c9f3ab28d6f1d1207e1a10dee7d87b5f8578a73
Guus van der Meer <info@guus.ninja>
2025-10-23 18:09:33 +0200

    Fixed webgo.de

--- src/data/rules.js
+++ src/data/rules.js
@@ -18748,6 +18748,7 @@ const rules = {
     s: 'pnl-cookie-wall-widget, pnl-cookie-widget-overview, pnl-cookie-widget-settings { display: none !important; } body[style*="position: fixed"][style*="overflow: hidden"] { position: static !important; overflow: auto !important; width: auto !important; height: auto !important; }',
   },
   "webgo.de": {
+    j: 5,
     s: "div.bg-elements-tertiary.z-backdrop { display: none !important; }",
   },
 
