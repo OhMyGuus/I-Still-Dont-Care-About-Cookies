857de6b7a462de6e2f7b86308db12d364ca07e7a
Guus van der Meer <info@guus.ninja>
2025-11-05 19:41:26 +0100

    fixed rules

--- src/data/rules.js
+++ src/data/rules.js
@@ -19642,9 +19642,7 @@ const rules = {
   "folkbiblioteken.lund.se": {
     s: "#cc--main,#cc_div,#cm-ov{display:none!important}html,body{overflow:auto!important}",
   },
-  "hugendubel.info": {
-    s: "dialog.confirm.cookie,dialog.confirm.cookie::backdrop{display:none!important;visibility:hidden!important;pointer-events:none!important}body{overflow:auto!important}",
-  },
+  "hugendubel.info": { j: 5 },
 
   // end of const rules
 };
