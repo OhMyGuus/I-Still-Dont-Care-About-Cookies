e2cb8662b37bcac0a20bc835cf4abfdf6e50eb67
Guus van der Meer <info@guus.ninja>
2025-11-16 03:06:55 +0100

    fixed rules

--- src/data/rules.js
+++ src/data/rules.js
@@ -19781,7 +19781,7 @@ const rules = {
   },
   "fothermo.com": { s: "#banner-wrapper{ display: none !important; }" },
   "hut.de": { s: "#c-cookie-banner{ display:none !important; }" },
-  
+
   // end of const rules
 };
 
