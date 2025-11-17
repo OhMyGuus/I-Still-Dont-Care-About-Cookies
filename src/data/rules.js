bbafe7e8564d31a93069b15e9593835f5e357a7e
Guus van der Meer <info@guus.ninja>
2025-11-17 22:33:05 +0100

    fixed rules

--- src/data/rules.js
+++ src/data/rules.js
@@ -3508,9 +3508,6 @@ const rules = {
   "hindustantimes.com": { j: "5" },
   "lyricsmode.com": { c: 1 },
   "recurly.com": { s: ".recurly-gdpr{display:none !important}" },
-  "pokewiki.de": {
-    s: "[id^=sp_message_container_]{display:none!important} :is(html,body).sp-message-open{overflow:visible!important}",
-  },
   "aa.com": { s: "#aa_optoutmulti-Modal{display:none !important}" },
   "lzinios.lt": { c: 1 },
   "heroku.com": { s: "#heroku-cookie-banner{display:none !important}" },
