8e859d443b20c0211e5e97af59e3c5a174d40108
Guus van der Meer <info@guus.ninja>
2025-12-02 21:00:31 +0100

    Fixed rules

--- src/data/rules.js
+++ src/data/rules.js
@@ -15539,6 +15539,7 @@ const rules = {
   "fakenewsfrance.com": {
     s: "#js-cc-modal{display:none !important} html{overflow-y: unset !important}",
   },
+  "123inkt.be": { s: "[data-action=CookiePopup]{display:none!important}" },
   "123inkt.nl": { s: "[data-action=CookiePopup]{display:none!important}" },
   "123tinta.es": { s: "[data-action=CookiePopup]{display:none!important}" },
   "123drukuj.pl": { s: "[data-action=CookiePopup]{display:none!important}" },
@@ -20391,7 +20392,6 @@ const rules = {
   },
   "hubsite365.com": { s: ".jss455{display:none!important}" },
   "theshortcut.com": { s: '[class*="cookieBanner"]{display:none!important}' },
-  "123inkt.be": { s: ".c-simple-popup{display:none!important;}" },
 
   // end of const rules
 };
