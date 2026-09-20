c991c76ff486308b57afac4dea39a3752f8ed0b2
qwis1 <egorsemenihin053@gmail.com>
2026-09-20 16:02:04 +0300

    Add rule for www.koncar.hr (fixes #24988)

--- src/data/rules.js
+++ src/data/rules.js
@@ -20591,6 +20591,9 @@ const rules = {
     s: ".pcb { display: none !important; } html, body { overflow: auto !important; }",
   },
   "itcodik.com": { s: "div[class*=z-[9999]] { display: none !important; }" },
+  "www.koncar.hr": {
+    s: ".cookie-bar-outside-wrapper { display: none !important; }",
+  },
 
   // end of const rules
 };
