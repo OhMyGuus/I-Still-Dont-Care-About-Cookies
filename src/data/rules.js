aeb87176b6ea002995c0fba23a9b2876c0eacc2d
qwis1 <egorsemenihin053@gmail.com>
2026-09-20 19:22:22 +0300

    Add rule for versicherungen.preisvergleich.de (fixes #26947)

--- src/data/rules.js
+++ src/data/rules.js
@@ -20591,6 +20591,9 @@ const rules = {
     s: ".pcb { display: none !important; } html, body { overflow: auto !important; }",
   },
   "itcodik.com": { s: "div[class*=z-[9999]] { display: none !important; }" },
+  "versicherungen.preisvergleich.de": {
+    s: "#truendo_container, .tru-overlay, .tru-cookie-dialog-wrapper { display: none !important; }",
+  },
 
   // end of const rules
 };
