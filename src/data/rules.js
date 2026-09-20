a7d66ab470c3c999d08d906743f509dd0c64e5a3
qwis1 <egorsemenihin053@gmail.com>
2026-09-20 13:42:13 +0300

    Add rule for itcodik.com (fixes #21710)

--- src/data/rules.js
+++ src/data/rules.js
@@ -20590,6 +20590,7 @@ const rules = {
   "smartial.net": {
     s: ".pcb { display: none !important; } html, body { overflow: auto !important; }",
   },
+  "itcodik.com": { s: "div[class*=z-[9999]] { display: none !important; }" },
 
   // end of const rules
 };
