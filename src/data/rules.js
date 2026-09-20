b99a6a5550dcce714e8191cd8fc31266dbf8a45e
qwis1 <egorsemenihin053@gmail.com>
2026-09-20 19:09:14 +0300

    Add rule for nosugarsugar.de (fixes #26949)

--- src/data/rules.js
+++ src/data/rules.js
@@ -20591,6 +20591,9 @@ const rules = {
     s: ".pcb { display: none !important; } html, body { overflow: auto !important; }",
   },
   "itcodik.com": { s: "div[class*=z-[9999]] { display: none !important; }" },
+  "nosugarsugar.de": {
+    s: ".offcanvas.js-offcanvas-singleton.offcanvas-modal, .offcanvas-backdrop { display: none !important; }",
+  },
 
   // end of const rules
 };
