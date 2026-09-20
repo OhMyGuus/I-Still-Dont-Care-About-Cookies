00dd49e21acba37cb577cd9620863e9f67c9d56e
qwis1 <egorsemenihin053@gmail.com>
2026-09-20 15:33:41 +0300

    Add rule for exhibitionnews.uk (fixes #25005)

--- src/data/rules.js
+++ src/data/rules.js
@@ -20591,6 +20591,7 @@ const rules = {
     s: ".pcb { display: none !important; } html, body { overflow: auto !important; }",
   },
   "itcodik.com": { s: "div[class*=z-[9999]] { display: none !important; }" },
+  "exhibitionnews.uk": { s: ".jconfirm { display: none !important; }" },
 
   // end of const rules
 };
